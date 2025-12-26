3959005b56df8cbffad57dc757b81e840dcf4770
Andrew Boyd <ab@ajwebsolutions.com>
2025-12-26 16:29:55 +1000

    AspNetClaims

---
+++ IdentityExample/Persistence/Configuration/AspNetClaimsConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IdentityExample.Persistence.Configuration;
+
+public class AspNetClaimsConfiguration : IEntityTypeConfiguration<AspNetClaims>
+{
+    public void Configure(EntityTypeBuilder<AspNetClaims> builder)
+    {
+        builder.ToTable("AspNetClaims");
+        builder.HasKey(e => e.ClaimValue);
+
+        builder.Property(e => e.ClaimValue).HasMaxLength(256).IsRequired();
+    }
+}
